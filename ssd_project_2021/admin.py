260e6ae40debb785983468dc7bbbd6a27c439b8d
Eyelessdude <bohdandudka@gmail.com>
2021-06-09 20:51:13 +0200

    Add School, Teacher, Student, Exam, Course entities to admin panel

--- ssd_project_2021/admin.py
+++ ssd_project_2021/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from ssd_project_2021.models import Course, Student, Teacher, School, Exam
+
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(School)
+admin.site.register(Exam)
