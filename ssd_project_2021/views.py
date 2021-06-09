bf2f32a3dd5ef0917b33a016e5e0e9d1cc06c3c2
Eyelessdude <bohdandudka@gmail.com>
2021-06-09 17:16:46 +0200

    Add raw homepage implementation

--- ssd_project_2021/views.py
+++ ssd_project_2021/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from ssd_project_2021.models import Course
+
 
 # Create your views here.
+def home_page(request):
+    courses = Course.objects.all()
+    context = {'list_of_courses': courses}
+
+    return render(request, 'home_page.html', context)
